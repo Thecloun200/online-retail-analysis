96ac3a2236c9d5cea533e2c89af357e16574c25e
Adham Hasan <104209001+Thecloun200@users.noreply.github.com>
2026-02-12 18:21:19 +0200

    Update analysis_queries.sql.sql

--- analysis_queries.sql.sql
+++ analysis_queries.sql.sql
@@ -35,4 +35,40 @@ WHERE Quantity > 0 AND UnitPrice > 0
 GROUP BY Country
 ORDER BY total_sales DESC;
 
+/* ===============================
+    Top customers in purchase 
+================================== */
+
+select CustomerID ,sum(Quantity*UnitPrice) as totalPurchase
+from online_retail
+where Quantity>0 And UnitPrice>0 AND CustomerID IS NOT NULL
+group by CustomerID
+order by totalPurchase Desc ;
+
+
+/* ===============================
+    Monthly Sales Trend
+================================== */
+SELECT 
+    YEAR(InvoiceDate) AS year,
+    MONTH(InvoiceDate) AS month,
+    SUM(Quantity * UnitPrice) AS monthly_sales
+FROM online_retail
+WHERE Quantity > 0 AND UnitPrice > 0
+GROUP BY YEAR(InvoiceDate), MONTH(InvoiceDate)
+ORDER BY monthly_sales DEsc;
+
+ /* ===============================
+    10 top products in revenue
+================================== */
+
+SELECT top 10 Description,
+       SUM(Quantity * UnitPrice) AS product_revenue
+FROM online_retail
+WHERE Quantity > 0 AND UnitPrice > 0
+GROUP BY Description
+ORDER BY product_revenue DESC;
+
+
+
 
